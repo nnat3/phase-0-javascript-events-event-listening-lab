7a22d66a6118d1f466b7492cd59569305ac428b7
Neathan Kungu <natekungu857@gmail.com>
2025-06-21 20:16:28 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
-function addingEventListener() {
+function addingEventListener(click) {
+    const input = document.querySelector('input#button');
+    input.addEventListener('click', function() {
+        alert('I was clicked!');
+    });
 }
